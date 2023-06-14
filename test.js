b6ae5d3410c541a5fa2a2af2fdb535bb9cf2e527
RajkumarBalimidi <135091475+RajkumarBalimidi@users.noreply.github.com>
2023-06-14 02:29:41 -0700

    Add files via upload
    frontend

---
+++ test.js
@@ -0,0 +1,11 @@
+function fun(){
+    a=document.getElementById("ok").value
+    if(a=="")
+    {
+        alert("please enter name")
+        
+    }
+    else{
+        alert("you Login successfully")
+    }
+}
